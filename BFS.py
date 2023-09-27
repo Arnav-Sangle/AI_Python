ca867e2a57ed651e45218268a5147434ef3c8faf
Arnav-Sangle <90949554+Arnav-Sangle@users.noreply.github.com>
2023-09-27 10:56:59 +0530

    user input accepts with goal test

--- BFS.py
+++ BFS.py
@@ -1,39 +1,63 @@
-graph = {
-            'A':['B', 'C'],
-            'B':['D', 'E'],
-            'C':['F', 'G'],
-            'D':['H', 'I'],
-            'H':[],
-            'I':[],
-            'E':['J', 'K'],
-            'J':[],
-            'K':[],
-            'F':['L', 'M'],
-            'L':[],
-            'M':[],
-            'G':['N', 'O'],
-            'N':[],
-            'O':[]
-        }
+# graph = {
+#             'A':['B', 'C'],
+#             'B':['D', 'E'],
+#             'C':['F', 'G'],
+#             'D':['H', 'I'],
+#             'H':[],
+#             'I':[],
+#             'E':['J', 'K'],
+#             'J':[],
+#             'K':[],
+#             'F':['L', 'M'],
+#             'L':[],
+#             'M':[],
+#             'G':['N', 'O'],
+#             'N':[],
+#             'O':[]
+#         }
 
-#Empty lists
+graph = {}
+n = int(input("Enter no. of nodes: "))
+
+print("Enter graph (Parent_Node : Child_Node)")
+for i in range(n):
+    parent = input("Parent Node: ")
+
+    print("Child Nodes: ")
+    childList = []
+    child = input("\t")
+    while child:
+        childList.append(child)
+        child = input("\t")
+
+    graph[parent] = childList
+
+print(graph)
+
 visited = []
-queue = []  
+queue = []
 
-def bfs(visited, graph, node):  #function for bfs
-    visited.append(node)
+
+def bfs(key, visited, graph, node):
+    print("Source node:", node)
+
+    visited.append(node)    # source node appended
     queue.append(node)
-    
+
+    print("Path:", end=" ")
     while queue:
-        m=queue.pop(0)
+        m = queue.pop(0)    # pop & print visited node
         print(m, end=" ")
-    
+        if m == key:
+            break
+
         for neighbour in graph[m]:
             if neighbour not in visited:
                 visited.append(neighbour)
                 queue.append(neighbour)
 
-#Driver code
-print("Following is the Breadth-First-Search")
-srcNode = input("Enter source node: ")
-bfs(visited, graph, srcNode)
+
+# Driver code
+key = input("Enter node to search: ")
+print("Performing BFS Goal Test-")
+bfs(key, visited, graph, 'A')
